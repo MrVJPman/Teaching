69c5e22502b6bbacf230b0101b4c61fc55be3abc
mrvjpman <bob.kong@mail.utoronto.ca>
2021-02-05 19:23:22 -0500

    Made solutions more efficient but still slow. Takes forever for test cases 02 to 09

--- CCC/Junior2020/j5_s2/CCC---2020---Junior---Q5.py
+++ CCC/Junior2020/j5_s2/CCC---2020---Junior---Q5.py
@@ -1,21 +1,21 @@
 #2020 Junior Q5
 
-def get_all_possible_coordinates_of_number(number, M, N, visited_coordinates):
+def get_all_possible_coordinates_of_number(number, M, N):
     coordinates_list_of_number = []
-#    print(number)
-    for index in range(1, number+1 // 2):
-        if (number % index == 0):
-#            print(list(set(coordinates_list_of_number)))
+    index = 0
+    while index * index <= number:
+        index += 1
+        if number % index == 0 :
+            if (index - 1) == ((number // index) - 1) and (index - 1) < M and (index - 1) < N:
+                coordinates_list_of_number.append( ((index - 1), (number // index) - 1))      
+                continue
             if (index - 1) < M and (number // index) - 1 < N:
-#                print("index - 1", index - 1)
-#                print("M", M)
                 new_coordinate = (index - 1, (number // index) - 1)
                 coordinates_list_of_number.append(new_coordinate)                
-            if (index - 1) < N and (number // index) - 1 < M:
+            if (number // index) - 1 < M and (index - 1) < N :
                 new_coordinate = ((number // index) - 1, index - 1)
                 coordinates_list_of_number.append(new_coordinate)
- #   print(list(set(coordinates_list_of_number)))
-    return list(set(coordinates_list_of_number))
+    return coordinates_list_of_number
     
 
 def CCC_2020_Question_5(input_string):
@@ -28,59 +28,31 @@ def CCC_2020_Question_5(input_string):
     
     M = int(lines[0])
     N = int(lines[1])
-    coordinates_values_list = [row.split() for row in lines[2:] ]
-    #print(coordinates_values_list)
-    
     
     coordinates_values_dict = {}
     values_coordinates_dict = {}
     for row in range(M):
         for value in range(N):
-            cell_number = int(lines[row+2].split()[value])
-            coordinates_values_dict[(row,value)] = cell_number
-            if cell_number not in values_coordinates_dict:
-                values_coordinates_dict[cell_number] = [(row,value)]
-            else:
-                values_coordinates_dict[cell_number].append((row,value))
+            coordinates_values_dict[(row,value)] = int(lines[row+2].split()[value])
             
 #    print(coordinates_values_dict)
-
-
-    #for row_of_list in coordinates_values_list:
-        #for index in range(int(N)):
-            #row_of_list[index] = int(row_of_list[index])
-#    print(coordinates_values_list)
     
     visited_coordinates = [(0, 0)]
     coordiates_to_check_queue = [(0, 0)]
     
     while coordiates_to_check_queue != []:
         coordinate_to_check = coordiates_to_check_queue.pop(0)
-#        print(coordinate_to_check)
         if coordinate_to_check == (M-1, N-1):
             return("yes")
         cc_x = coordinate_to_check[0]
         cc_y = coordinate_to_check[1]
-        for potential_coordinate in get_all_possible_coordinates_of_number(coordinates_values_dict[(cc_x, cc_y)], M, N, visited_coordinates):
+        for potential_coordinate in get_all_possible_coordinates_of_number(coordinates_values_dict[(cc_x, cc_y)], M, N):
             if potential_coordinate in visited_coordinates:
                 continue
             else: 
                 coordiates_to_check_queue.append(potential_coordinate)
                 visited_coordinates.append(potential_coordinate)
-
-#            if coordinates_values_dict[(cc_x, cc_y)] == (potential_coordinate[0] + 1) * (potential_coordinate[1] + 1):
     return ("no")
-
-'''        for row in range(M):
-            for value in range(N):
-                if (row, value) in visited_coordinates:
-                    continue
-                if coordinates_values_list[cc_x][cc_y] == (row+1)*(value+1) :
-#                    print(coordinates_values_list[cc_x][cc_y])
-                    new_coordinate = (row, value)
-                    coordiates_to_check_queue.append(new_coordinate)
-                    visited_coordinates.append(new_coordinate)
-#                    print("new_coordinate", new_coordinate)'''
     #return str(None) #This is here by default. Don't forget to comment this
     #Your Solution Ends Here
 
@@ -120,7 +92,7 @@ def run_all_test_cases(test_dict):
         print("--------------------TEST CASE: ", testcase, "--------------------")
         input_string = combined_file_contents[testcase][0]
         print("----------Input:  ")
-#        print(input_string)
+        #print(input_string)
         received_output = CCC_2020_Question_5(input_string) #CHANGE NAME OF THIS FUNCTION
         expected_output = combined_file_contents[testcase][1].strip()       
         print("----------Expected: ")
