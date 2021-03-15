e97b8be3d592ab451336bda6090c22bca2c477cf
mrvjpman <bob.kong@mail.utoronto.ca>
2021-03-15 15:23:06 -0400

    finished ccc 2018 j1

---
+++ CCC/Junior2018/J1/CCC---2018---Junior---Q1.py
@@ -0,0 +1,87 @@
+def CCC_2018_Junior_Question_1(input_string):
+    #this will always be here strip()
+    lines = input_string.strip().split("\n") 
+    #strip : to remove last newline
+    #split : to divide into individual lines
+
+    digit_1 = int(lines[0])
+    digit_2 = int(lines[1])
+    digit_3 = int(lines[2])
+    digit_4 = int(lines[3])
+
+    #Your Solution Starts Here 
+    solution_string = None    
+    if digit_1 in [8, 9] and digit_4 in [8, 9] and digit_2 == digit_3:
+        solution_string = "ignore"
+    else:
+        solution_string = "answer"
+    #Your Solution Ends Here
+    return str(solution_string) 
+
+
+#============================START : READ .in and .out file contents============================
+import os 
+
+all_input_file_contents = {}
+all_output_file_contents = {}
+for filename in os.listdir("."):
+    if "in" in filename:
+        opened_file = open(filename)
+        opened_file_contents = opened_file.read()
+        all_input_file_contents[filename] = opened_file_contents
+    elif "out" in filename:
+        opened_file = open(filename)
+        opened_file_contents = opened_file.read()
+        all_output_file_contents[filename] = opened_file_contents
+
+#print(all_input_file_contents)
+#print(all_output_file_contents)
+
+combined_file_contents = {}
+for content in all_input_file_contents:
+    combined_file_contents[content[:-3]] = [all_input_file_contents[content], all_output_file_contents[content[:-3]+".out"]]
+#print(combined_file_contents)
+
+
+#============================END : READ .in and .out file contents============================
+
+
+#test_dict structure
+#key is test case name
+#value list item 0 is inputs
+#value list item 1 is outputs
+#{'s2.1': ['1\n1\n327707\n928587\n', '928587\n']}
+def run_all_test_cases(test_dict):
+    for testcase in test_dict:
+        print("--------------------TEST CASE: ", testcase, "--------------------")
+        input_string = combined_file_contents[testcase][0]
+        print("----------Input:  ")
+        print(input_string)
+        received_output = CCC_2018_Junior_Question_1(input_string) #CHANGE NAME OF THIS FUNCTION
+        expected_output = combined_file_contents[testcase][1].strip()       
+        print("----------Expected: ")
+        print(expected_output)
+        print("----------Received: ")
+        print(received_output)
+        assert(expected_output == received_output)
+        print("\n")
+    print("CONGRATULATIONS!!! ALL TEST CASES PASS!")
+
+#Comment/uncomment this as needed
+run_all_test_cases(combined_file_contents)
+
+#This section is where you implement user input handling
+print("MANUALLY ENTER TEST CASES HERE")
+print("Enter inputs:")
+user_input_string = ""
+#START : insert your user input handling code below
+user_input_string = input() + "\n"
+user_input_string = input() + "\n"
+user_input_string = input() + "\n"
+user_input_string = input() + "\n"
+#END : insert your user input handling code above
+print("user_input_string:")
+print(user_input_string)
+print("result:")
+print(CCC_2018_Junior_Question_1(user_input_string)) 
+#CHANGE NAME OF ABOVE FUNCTION
